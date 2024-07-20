1c7d38a59c24d95b225f227ed46c6c89536ec88d
artuzam <charliecazc@gmail.com>
2024-07-19 18:19:58 -0600

    Hero section WIP

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'accent-green': '#7BB500',
+				'background-green': '#F2FFD8',
+				'accent-blue': '#406AFF',
+				'background-blue': '#D8E0FF',
+				'accent-pink': '#FD38E9',
+				'background-pink': '#FFD8FB'
+			}
+		},
 	},
 	plugins: [],
 }
