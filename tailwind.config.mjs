446d439fc1294b7a952895d0f8920350ef956d75
artuzam <charliecazc@gmail.com>
2024-07-25 17:17:18 -0600

    Fixed app

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'accent-green': '#7BB500',
-				'background-green': '#F2FFD8',
-				'accent-blue': '#406AFF',
-				'background-blue': '#D8E0FF',
-				'accent-pink': '#FD38E9',
-				'background-pink': '#FFD8FB'
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'accent-green': '#7BB500',
+        'background-green': '#F2FFD8',
+        'accent-blue': '#406AFF',
+        'background-blue': '#D8E0FF',
+        'accent-pink': '#FD38E9',
+        'background-pink': '#FFD8FB',
+      },
+    },
+  },
+  plugins: [require('tailwindcss-3d')],
+};
